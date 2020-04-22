c7980c62a7e3eced994aca1fbea2bcb64ff93b58
Oleg Polyakov <olegpolyakov@outlook.com>
2020-04-22 23:35:17 +0300

    Init firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp, firestore, auth } from 'firebase';
+
+initializeApp({
+    apiKey: 'AIzaSyC6TtRsHffm2TwQCb7lUcXLDJh5N_gMSHE',
+    authDomain: 'codelibrary-47708.firebaseapp.com',
+    databaseURL: 'https://codelibrary-47708.firebaseio.com',
+    projectId: 'codelibrary-47708',
+    storageBucket: 'codelibrary-47708.appspot.com',
+    messagingSenderId: '212510535763',
+    appId: '1:212510535763:web:561c6f91929c2663d856a6',
+    measurementId: 'G-Y0XWSFBXDR'
+});
+
+const db = firestore();
+const auth = auth();
+
+export { db, auth };
