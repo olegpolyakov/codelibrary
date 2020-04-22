3394ae81fbfb31e8fd06b581d4da58436ff2772e
Oleg Polyakov <olegpolyakov@outlook.com>
2020-04-22 23:45:54 +0300

    Update index.js Add Router and Provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import './index.scss';
+
+import store from './store';
 import App from './App';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
     document.getElementById('root')
 );
