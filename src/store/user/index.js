209aee4c74fb652b736520018a531074025fd1d2
Oleg Polyakov <olegpolyakov@outlook.com>
2020-05-05 23:10:02 +0300

    Update store user

--- src/store/user/index.js
+++ src/store/user/index.js
@@ -1,3 +1,27 @@
 export default function reducer(state = null, action) {
-    return state;
+    switch (action.type) {
+        case 'LOGIN_USER':
+            return action.payload.user;
+
+        case 'LOGOUT_USER':
+            return null;
+
+        default:
+            return state;
+    }
+}
+
+export function login(user) {
+    return {
+        type: 'LOGIN_USER',
+        payload: {
+            user
+        }
+    };
+}
+
+export function logout() {
+    return {
+        type: 'LOGOUT_USER'
+    };
 }
