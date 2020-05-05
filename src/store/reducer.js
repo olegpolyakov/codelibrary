313b2da78666985318c66d26a2591a32d84e2bf9
Oleg Polyakov <olegpolyakov@outlook.com>
2020-05-05 23:10:28 +0300

    Update store reducers index

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,8 +2,12 @@ import { combineReducers } from 'redux';
 
 import user from './user';
 import books from './books';
+import lists from './lists';
+import topics from './topics';
 
 export default combineReducers({
     user,
-    books
+    books,
+    lists,
+    topics
 });
